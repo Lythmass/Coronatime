b13da3ee007350b0c0835642477e55186db0d552
Lythmass <lythmass17@gmail.com>
2022-11-30 22:44:10 +0400

    feat: add languages to registration page
    refactoring javascript to validate. adding 2 languages for registration page. creating 2 buttons to switch between languages.

---
+++ resources/lang/en/register.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'welcome'         => 'Welcome to Coronatime',
+	'enter-info'      => 'Please enter required info to sign up',
+	'username'        => 'Username',
+	'enter-username'  => 'Enter unique username',
+	'email'           => 'Email',
+	'enter-email'     => 'Enter your email',
+	'password'        => 'Password',
+	'enter-password'  => 'Fill in password',
+	'repeat-password' => 'Repeat password',
+	'remember'        => 'Remember this device',
+	'signup'          => 'SIGN UP',
+	'ask'             => 'Already have an account?',
+	'login'           => 'Log in',
+];
