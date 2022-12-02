2bba972c68abae50e3a5e86177b6c58dcc29814c
Lythmass <lythmass17@gmail.com>
2022-12-02 14:49:08 +0400

    style(login): fix design issues
    add forgot password link, fix sign up for free button.

--- resources/views/components/session-link.blade.php
+++ resources/views/components/session-link.blade.php
@@ -1,1 +1,5 @@
-<p class = "self-center text-zinc-500">{!!$text!!} <span class = "text-black font-bold"><a href = "{{route('login-user', [app()->getLocale()])}}"> {{$action}}</a></span> </p>
+@if (request()->route()->getName() == 'login-user')
+    <p class = "self-center text-zinc-500">{!!$text!!} <span class = "text-black font-bold"><a href = "{{route('create-user', [app()->getLocale()])}}"> {{$action}}</a></span> </p>
+@else
+    <p class = "self-center text-zinc-500">{!!$text!!} <span class = "text-black font-bold"><a href = "{{route('login-user', [app()->getLocale()])}}"> {{$action}}</a></span> </p>
+@endif
