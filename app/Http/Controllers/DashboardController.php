5d473d3d0a7fe9a3494a5e908f32e3107e479029
Lythmass <lythmass17@gmail.com>
2022-12-08 18:03:51 +0400

    feat:add search functionality
    add search for both languages.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Statistic;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
 	public function index($locale, $panel)
 	{
 		$statistics = Statistic::all();
+
 		$newCases = $statistics->sum('confirmed');
 		$recovered = $statistics->sum('recovered');
 		$death = $statistics->sum('death');
@@ -24,6 +26,12 @@ class DashboardController extends Controller
 
 		if ($panel == 'bycountry')
 		{
+			if (request('search') ?? false)
+			{
+				$statistics = DB::table('statistics')
+				->join('countries', 'countries.code', '=', 'statistics.code')
+				->where('countries.' . $locale, 'like', '%' . request()->input('search') . '%')->get();
+			}
 			return view('verified.dashboard', [
 				'statistics' => $statistics,
 				'newCases'   => $newCases,
