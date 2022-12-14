b3b07ff3c7ce1cdc7aeae82b048fb583b88aab77
Lythmass <lythmass17@gmail.com>
2022-12-14 13:21:14 +0400

    fix: use http fake
    use http fake to create fake data and check if the database is filled or not.

--- tests/Feature/FetchDataTest.php
+++ tests/Feature/FetchDataTest.php
@@ -4,23 +4,35 @@ namespace Tests\Feature;
 
 use App\Models\Country;
 use App\Models\Statistic;
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class FetchDataTest extends TestCase
 {
 	public function test_check_if_coronatime_fetch_data_command_works()
 	{
-		$this->artisan('coronatime:fetch-data')->expectsOutput('Successfully fetched data.');
-	}
+		Http::fake([
+			'https://devtest.ge/countries' => Http::response([[
+				'code' => 'GE',
+				'name' => [
+					'en'   => 'Georgia',
+					'ka'   => 'საქართველო',
+				],
+			]]),
+			'https://devtest.ge/get-country-statistics' => Http::response([
+				'country'    => 'Georgia',
+				'code'       => 'GE',
+				'confirmed'  => 4,
+				'recovered'  => 4,
+				'deaths'     => 0,
+			]),
+			function (Request $request) {
+				return $this->assertTrue(Country::exists()) && $this->assertTrue(Statistic::exists());
+			},
+		]);
 
-	public function test_check_if_the_database_has_been_filled_by_country_data()
-	{
-		$this->assertTrue(Country::exists());
-	}
-
-	public function test_check_if_the_database_has_been_filled_by_statistics_data()
-	{
-		$this->assertTrue(Statistic::exists());
+		$response = $this->artisan('coronatime:fetch-data')->expectsOutput('Successfully fetched data.');
 	}
 
 	public function test_check_the_connection_between_countries_and_statistics_tables()
